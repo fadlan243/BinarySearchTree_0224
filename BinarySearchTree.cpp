0cef08df08f441999ef289252c7a4bae5208d9ee
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-24 10:12:27 +0700

    inisialisasi right child dan left child ke null

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,4 +4,13 @@ using namespace std;
 class Node
 {
 public:
-   
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node calss
+    Node()
+    {
+        leftchild = nullptr; /// Initialize left child to null
+        rightchild = nullptr; // initialize right child to null
+    }
