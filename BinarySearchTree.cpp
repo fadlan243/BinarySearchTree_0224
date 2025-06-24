58a60e1448a0f3fdb02dcd42ca46a804bc9a01f9
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-24 11:07:27 +0700

    void insert

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,14 +8,13 @@ public:
     Node *leftchild;
     Node *rightchild;
 
-    // Constructor for the node calss
+    // Constructor for the node class
     Node()
     {
         leftchild = nullptr; /// Initialize left child to null
         rightchild = nullptr; // initialize right child to null
     }
 };
-
 class BinaryTree
 {
 public:
@@ -23,7 +22,59 @@ public:
 
     BinaryTree()
     {
-        ROOT = nullptr; // initialize ROOT to null
+        ROOT = nullptr; // Initialize ROOT to null
     }
 
-    
+    void insert()
+    {
+        int x;
+        cout << "Masukan nilai: ";
+        cin >> x;
+
+        // step 1: Allocate memory for the node 
+        Node *newNode = new Node();
+
+        // step 2: Assign value to the data field of new node
+        newNode->info = x;
+
+        // step 3: Make the left and the right child of the new node point to null 
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // step 4: Locate the node wich will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
+        // step 5: if parent is NULL (tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a: Mark the node ad ROOT
+            ROOT = newNode;
+            
+            // 5b: exit
+            return;
+        }
+
+        // step 6: if the value in the data field od new node is less than that of parent 
+        if (x < parent->info)
+        {
+            // 6a: make the left of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b: exit
+            return;
+        }
+
+        // step 7: if the value in the data field of the new node is greater than that of the parent 
+        else if ( x = parent->info)
+        {
+            // 7a: make the right child of parent pointer to the new node 
+            parent->rightchild = newNode;
+
+            // 7b: exit 
+            return;
+        }
+    }
+
+   
